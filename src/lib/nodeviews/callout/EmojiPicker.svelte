cbcd58da1aaea6eb5361beecd3c695a4d7fee8d5
SupunKavinda <supunkavinda1125@gmail.com>
2025-09-18 12:06:00 +0530

    callout done

---
+++ src/lib/nodeviews/callout/EmojiPicker.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { EmojiPicker } from '@hyvor/design/components';
+
+	interface Props {
+		emoji: string | undefined;
+		changeAttr: (name: string, value: string) => void;
+	}
+
+	let { emoji, changeAttr }: Props = $props();
+
+	function handleSelect(e: string) {
+		changeAttr('emoji', e);
+	}
+</script>
+
+<EmojiPicker
+	{emoji}
+	onselect={handleSelect}
+	removable
+	iconButtonSize="small"
+	iconButtonColor="input"
+	iconButtonVariant="invisible"
+/>
