f2dec295cd255b409e3347f27365b16352b89156
SupunKavinda <supunkavinda1125@gmail.com>
2026-09-03 10:17:26 +0200

    fixes bug: suggestion panel docking when the editor is not 100% page height Fixes #69

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,14 @@
 import { mount } from 'svelte'
 import App from './App.svelte'
 import DiffPage from './DiffPage.svelte'
+import ScrollTestPage from './ScrollTestPage.svelte'
 
 const isDiffPage = window.location.pathname.startsWith('/diff')
+const isScrollTestPage = window.location.pathname.startsWith('/scroll-test')
 
-const app = mount(isDiffPage ? DiffPage : App, {
+const page = isDiffPage ? DiffPage : isScrollTestPage ? ScrollTestPage : App
+
+const app = mount(page, {
   target: document.getElementById('app')!,
 })
 
